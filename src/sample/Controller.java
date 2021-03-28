a3ce27c9425f4b66ca305e71923c87a26e44b263
Malav Doshi <doshimalav7@gmail.com>
2021-03-28 09:46:21 -0400

    initial

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,10 +1,15 @@
 package sample;
 
 
+import com.sun.javafx.logging.Logger;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -21,7 +26,35 @@ public class Controller {
 
     @FXML
     public void goToCoffeeTab(){
-
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("Coffee.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 750, 600);
+            scene.getStylesheets().add(getClass().getResource("MainWindow.css").toExternalForm());
+            Stage stage = new Stage();
+            stage.setTitle("Order Coffee");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    @FXML
+    public void goToDonutTab(){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("Donut.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 750, 600);
+            scene.getStylesheets().add(getClass().getResource("MainWindow.css").toExternalForm());
+            Stage stage = new Stage();
+            stage.setTitle("Order Donuts");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Donut.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Order Items");
         Scene scene = new Scene(root, 750, 600);
         scene.getStylesheets().add(getClass().getResource("MainWindow.css").toExternalForm());
