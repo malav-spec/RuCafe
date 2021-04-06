00e4a6d8c2edfe56b0f3d4c41c9bb1f73a601350
Malav Doshi <doshimalav7@gmail.com>
2021-04-06 11:35:43 -0400

    Updated images path

---
+++ src/GUI/AllOrdersContoller.java
@@ -0,0 +1,103 @@
+package GUI;
+
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import static GUI.Main.allOrder;
+
+/**
+ * Used to represent Controller for all orders GUI
+ * @author Malav Doshi and Herik Patel
+ */
+public class AllOrdersContoller {
+    /**
+     * Used to add values to Listview
+     */
+    @FXML
+    private ListView orderList;
+    /**
+     * Button for the GUI
+     */
+    @FXML
+    private Button add,change;
+
+    public void initialize(){
+        ArrayList<String> temp = allOrder.makeAL();
+        orderList.setItems(FXCollections.observableArrayList(temp));
+    }
+
+
+
+    /**
+     * Used to export all orders to a file
+     */
+    public void fileExport() {
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Save File");
+        File file = chooser.showSaveDialog(new Stage());
+        if(file == null){
+            return ;
+        }
+        String path = file.getAbsolutePath();
+        exportDatabase(path);
+    }
+
+    /**
+     * Writes all data to a given file
+     * @param path Destination where the file is stored
+     */
+    private void exportDatabase(String path){
+
+        if(path == null){
+            return;
+        }
+
+        File myObj = new File(path);
+        ArrayList<String> temp = allOrder.makeAL();
+        try {
+            FileWriter myWriter = new FileWriter(path);
+            for(int i = 0; i <temp.size() ; i++)
+            {
+                myWriter.write(temp.get(i));
+                myWriter.write('\n');
+
+            }
+            myWriter.close();
+        }
+        catch (IOException e) {
+        }
+
+    }
+
+    /**
+     * Used to remove data from storeOrder
+     */
+    public void remove(){
+        if(orderList.getSelectionModel().getSelectedItem() == null){
+            return;
+        }
+        String index = ""+orderList.getSelectionModel().getSelectedIndex();
+        allOrder.remove(index);
+        ArrayList<String> temp = allOrder.makeAL();
+        orderList.setItems(FXCollections.observableArrayList(temp));
+    }
+
+
+
+    /**
+     * Used to navigate back to main screen
+     * @throws Exception throws Scene not found exception
+     */
+    public void MainScreen()throws Exception{
+        Stage stage=(Stage) add.getScene().getWindow();
+        stage.close();
+    }
+
+}
---
+++ src/GUI/Main.java
@@ -0,0 +1,31 @@
+package GUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import model.Order;
+import model.StoreOrder;
+
+public class Main extends Application {
+    public static Order currentOrder = new Order();
+    public static StoreOrder allOrder = new StoreOrder();
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Order Items");
+        Scene scene = new Scene(root, 750, 600);
+        scene.getStylesheets().add(getClass().getResource("MainWindow.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
